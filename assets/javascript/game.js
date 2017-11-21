f65ef5980c73bdfab84e276b2a7f67703b660922
kaktuxmx <kaktuxsql@gmail.com>
2017-11-20 18:54:00 -0700

    clicks work now, i am doing the logic of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,48 +9,65 @@ var ranimg4;
 //This is to create all the random numbers that are going to be used.
 $(document ).ready(function(){
 // Create all random numbers
-randomtotalnm = Math.floor(Math.random() * 40 + 35);
+var randomtotalnm = Math.floor(Math.random() * 40 + 35);
 		console.log(randomtotalnm);
 		$("#numerototal1").html("This is the number that you need to match:" + randomtotalnm);
 
-ranimg1 = Math.floor(Math.random() * 15 )+ 1;
+var ranimg1 = Math.floor(Math.random() * 15 )+ 1;
 		console.log(ranimg1);
 		//$("#azteca1").html(ranimg1);
 
-ranimg2 = Math.floor(Math.random() * 10) + 1;
+var ranimg2 = Math.floor(Math.random() * 10) + 1;
 		console.log(ranimg2);
 		//$("#cantante1").html("");
 
-ranimg3 = Math.floor(Math.random() * 3) + 1;
+var ranimg3 = Math.floor(Math.random() * 3) + 1;
 		console.log(ranimg3);
 		//$("#catrin1").html("");
 
-ranimg4 = Math.floor(Math.random() * 9)+ 1;
+var ranimg4 = Math.floor(Math.random() * 9)+ 1;
 		console.log(ranimg4);
 		//$("#pistola1").html("");
 
 
 
-})
 
-var n;
+
+var ntotal = 0;
 
 // 1. Pick the random amounts with the clicks on the images and add up the addition on the screen
 
 //verifing if i can do this... with the n variables... still not making my n to wokr on the HTML...
-		$("#foto1").on("click", n)
-		$("#foto2").on("click", n2)
-		$("#foto3").on("click", n3)
-		$("#foto4").on("click", n4)
+		$("#foto1").on("click", function(){
+				ntotal += ranimg1;
+				console.log(ntotal);
+				$("#valordenumero").html("The number that you have so far is:" + ntotal);
+		});
+		$("#foto2").on("click", function(){
+				ntotal += ranimg2;
+				$("#valordenumero").html("The number that you have so far is:" + ntotal);
+		});
+		$("#foto3").on("click", function(){
+				ntotal += ranimg3;
+				console.log(ntotal);
+				$("#valordenumero").html("The number that you have so far is:" + ntotal);
+		});
+		$("#foto4").on("click", function(){
+				ntotal += ranimg4;
+				$("#valordenumero").html("The number that you have so far is:" + ntotal);
+		});
+				
 
 		function addingnumber() {
-			n += this.value;
-			$("#valordenumero")html("Esto es lo que llevas sumado:" + n);
+			if (ntotal === randomtotalnm) {
+					$("#yaganaste").html("GANASTE!!!!!!")
+				}
+				
 		}
 
 // 2. compare results when the equal or over values are on the addition
 // 3. System to save the score of the player.
-
+});
 
 
 
