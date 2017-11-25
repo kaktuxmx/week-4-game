842cdd556781015e8e235faf52d38ee28b541e2f
kaktuxmx <kaktuxsql@gmail.com>
2017-11-24 23:31:43 -0700

    finish js code, need css fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,73 @@
+//This is to create all the random numbers that are going to be used.
+$(document ).ready(function(){
+
+//Game Variables
 var randomtotalnm = "";
 var ranimg1;
 var ranimg2;
 var ranimg3;
 var ranimg4;
 
+//Score Variables
+var ntotal = 0;
+var scorewin = 0;
+var scoreloose = 0;
+var jjs = 0;
 
 
-//This is to create all the random numbers that are going to be used.
-$(document ).ready(function(){
+	function getrandomnumber(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	function resetgame() {
+		// I need to reset the Game randomtotalnm
+		randomtotalnm = getrandomnumber(65,95);
+		$("#numerototal1").html("This is the number that you need to match:" + randomtotalnm);
+
+		// I need to reset the random numbers of the images
+		ranimg1 = getrandomnumber(5,12);
+		console.log(ranimg1);
+		ranimg2 = getrandomnumber(2,4);
+		console.log(ranimg2);
+		ranimg3 = getrandomnumber(5,15);
+		console.log(ranimg3);
+		ranimg4 = getrandomnumber(1,7);
+		console.log(ranimg4);
+		$("#foto1").attr("ran", ranimg1);
+		$("#foto2").attr("ran", ranimg2);
+		$("#foto3").attr("ran", ranimg3);
+		$("#foto4").attr("ran", ranimg4);
+		// I need to reset the tag #yaganaste
+		$("#yaganaste").html("");
+
+		// I need to reset ntotal 
+		ntotal = 0;
+		$("#valordenumero").html("The number that you have so far is:" + ntotal);
+	}
+
 // Create all random numbers
-var randomtotalnm = Math.floor(Math.random() * 40 + 35);
+var randomtotalnm = getrandomnumber(65, 90);
 		console.log(randomtotalnm);
 		$("#numerototal1").html("This is the number that you need to match:" + randomtotalnm);
 
-var ranimg1 = Math.floor(Math.random() * 15 )+ 1;
+var ranimg1 = getrandomnumber(5, 12);;
 		console.log(ranimg1);
 		//$("#azteca1").html(ranimg1);
 
-var ranimg2 = Math.floor(Math.random() * 10) + 1;
+var ranimg2 = getrandomnumber(2, 4);;
 		console.log(ranimg2);
 		//$("#cantante1").html("");
 
-var ranimg3 = Math.floor(Math.random() * 3) + 1;
+var ranimg3 = getrandomnumber(5, 15);;
 		console.log(ranimg3);
 		//$("#catrin1").html("");
 
-var ranimg4 = Math.floor(Math.random() * 9)+ 1;
+var ranimg4 = getrandomnumber(1, 7);;
 		console.log(ranimg4);
 		//$("#pistola1").html("");
 
 
 
-var ntotal = 0;
-var scorewin = 0;
-var scoreloose = 0;
 
 
 		$("#foto1").attr("ran", ranimg1);
@@ -51,24 +85,26 @@ var scoreloose = 0;
 
 		function addingnumber() {
 			if (ntotal > randomtotalnm) {
-					$("#yaganaste").html("You are over!");
-					scoreloose++;
-					$("#scoreloose").html("Your loose games: " + scoreloose);
-					}
-
-			else if (ntotal < randomtotalnm) {
-					$("#yaganaste").html("Keep playing, you still not there!");
-				}
-
-			else if (ntotal === randomtotalnm) {
-					$("#yaganaste").html("GANASTE!!!!!!")
-					score++
-					$("#scoreganando").html("You score is:" + scoreganando);
-				}
-			};
-
-// 2. compare results when the equal or over values are on the addition
-// 3. System to save the score of the player.
+				$("#yaganaste").html("You are over!");
+				scoreloose++;
+				jjs++;
+				$("#juegosjugados").html("Juegos jugados=" + jjs);
+				$("#scoreperdiendo").html("Your loose games: " + scoreloose);
+				// Create a funtion where the game will reset, but only will reset the Total Number, the guessed numbers and there should be a game counter too.
+				resetgame();
+			}else if (ntotal < randomtotalnm) {
+				$("#yaganaste").html("Keep playing, you still not there!");
+			}else if (ntotal === randomtotalnm) {
+				$("#yaganaste").html("WIIINNNNNN!!!!!!");
+				scorewin++;
+				jjs++;
+				$("#juegosjugados").html("Juegos jugados=" + jjs);
+				$("#scoreganando").html("You score is:" + scorewin);
+				resetgame();
+			}
+		};
+			
+		
 });
 
 
@@ -98,3 +134,7 @@ var scoreloose = 0;
 		// 		ntotal += ranimg4;
 		// 		$("#valordenumero").html("The number that you have so far is:" + ntotal);
 		// });
+
+		// 2. compare results when the equal or over values are on the addition
+// 3. System to save the score of the player.
+
